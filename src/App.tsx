f79de8170345438af41b63eef694e3c29dcba3b7
lxiiiixi <104674636+lxiiiixi@users.noreply.github.com>
2024-06-29 15:43:15 +0800

    Use telegram-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import twaLogo from './assets/tapps.png'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import "@telegram-apps/telegram-ui/dist/styles.css";
+import { AppRoot, Button, Placeholder } from "@telegram-apps/telegram-ui";
+import WebApp from "@twa-dev/sdk";
 
-import WebApp from '@twa-dev/sdk'
+// https://tgui.xelene.me/?path=/docs/blocks-card--documentation
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <div>
-        <a href="https://ton.org/dev" target="_blank">
-          <img src={twaLogo} className="logo" alt="TWA logo" />
-        </a>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>TWA + Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-      {/*  */}
-      <div className="card">
-        <button onClick={() => WebApp.showAlert(`Hello World! Current count is ${count}`)}>
-            Show Alert
-        </button>
-      </div>
-    </>
-  )
+    return (
+        <AppRoot>
+            <div>
+                <Placeholder header="Title" description="Description">
+                    <img
+                        alt="Telegram sticker"
+                        src="https://xelene.me/telegram.gif"
+                        style={{ display: "block", width: "144px", height: "144px" }}
+                    />
+                </Placeholder>
+                <Button>Button</Button>
+            </div>
+            <h1>TWA + Vite + React</h1>
+            <div className="card">
+                <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
+            </div>
+            {/*  */}
+            <div className="card">
+                <button onClick={() => WebApp.showAlert(`Hello World! Current count is ${count}`)}>
+                    Show Alert
+                </button>
+            </div>
+        </AppRoot>
+    );
 }
 
-export default App
+export default App;
